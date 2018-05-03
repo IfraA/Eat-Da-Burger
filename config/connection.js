4e3a6294e78b93a88fb6aa80aa68a39c74b509b7
IfraA <ifraa.aijaz@gmail.com>
2018-05-02 22:35:01 -0700

    db

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,18 @@ var connection = mysql.createConnection({
     database: "burger_db"
 });
 
+//jawsDB
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "hacktheplanet",
+        database: "todoagain_db",
+    });
+};
+
 // Make connection.
 connection.connect(function (err) {
     if (err) {
